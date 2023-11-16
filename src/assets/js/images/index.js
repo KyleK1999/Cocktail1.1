5a6b08da2d20b5a9e607583929dbd370015b9653
Kyle <kylekibbe99@gmail.com>
2023-11-15 16:54:09 -0800

    added some css updates and fixing html

--- src/assets/js/images/index.js
+++ src/assets/js/images/index.js
@@ -1,62 +1,52 @@
 window.addEventListener("DOMContentLoaded", getAndRenderRandomDrink);
 
-//fetch function
-
+// URL definitions
 const url = "https://www.thecocktaildb.com/api/json/v1/1/";
-const randomDrinkURL = "/random.php"
-const alcoholic = "/filter.php?a=Alcoholic"
-const nonalcoholic = "/filter.php?a=Non_Alcoholic"
+const randomDrinkURL = "random.php";
+const alcoholicFilterURL = "filter.php?a=Alcoholic";
+const nonAlcoholicFilterURL = "filter.php?a=Non_Alcoholic";
 
-function allDrinks(drinkURL) {
+// Fetch functions
+function fetchDrink(drinkURL) {
     return fetch(drinkURL)
       .then(response => response.json())
-      .then(data => data.drinks[0].strDrinkThumb); //extract img src 
+      .then(data => data.drinks[0]);
 }
+
 function getRandomDrink(){
-  return fetch(url + randomDrinkURL)
-  .then(response => response.json())
-  .then(data => data.drinks[0]);
+  return fetchDrink(url + randomDrinkURL);
 }
 
-//initializers
-
-allDrinks(url + randomDrinkURL).then((drinksArray) => {
-    //render all drinks)
-console.log(drinksArray)
+// Initial fetch and log
+fetchDrink(url + randomDrinkURL).then(drinksArray => {
+    console.log(drinksArray);
 });
 
-//dom selectors
-const fetchButton = document.querySelector("button")
-const hoverButton = document.querySelector("button")
+// DOM selectors
+const fetchButton = document.querySelector("button");
+const hoverButton = document.querySelector("button");
 const drinkImage = document.getElementById("DrinkImage");
 const drinkDescription = document.getElementById("drinkDescription");
-const drinkInstructions = document.getElementById("strInstructions")
+const drinkInstructions = document.getElementById("strInstructions");
 
-
-//event listeners
-hoverButton.addEventListener("mouseover", (event) => {
+// Event listeners
+hoverButton.addEventListener("mouseover", () => {
     hoverButton.style.color = "red";
-  });
+});
 
-  hoverButton.addEventListener("mouseout", (event) => {
+hoverButton.addEventListener("mouseout", () => {
     hoverButton.style.color = "white";
-  });
+});
 
-  fetchButton.addEventListener("click", () => {
+fetchButton.addEventListener("click", () => {
     getRandomDrink().then(drinkObj => {
       renderRandomDrink(drinkObj);
     });
-  });
+});
 
-
-
-
-
-
-
-//render functions
+// Render functions
 function renderRandomDrink(drinkObj) {
-  console.log(" ~ file: index.js:56 ~ renderRandomDrink ~ drinkObj:", drinkObj)
+  console.log("Rendered Drink:", drinkObj);
   drinkImage.src = drinkObj.strDrinkThumb;
   drinkDescription.textContent = drinkObj.strDrink;
   drinkInstructions.textContent = drinkObj.strInstructions;
@@ -65,12 +55,5 @@ function renderRandomDrink(drinkObj) {
 function getAndRenderRandomDrink() {
   getRandomDrink().then(drinkObj => {
     renderRandomDrink(drinkObj);
-  })
+  });
 }
-//{
-//    "ID": 2,
-//   "Drink Name": "Old Fashioned",
-//   "Image URL": "https://www.knobcreek.com/sites/default/files/2018-06/thumb__0010_8909_KC_Classic_Old_Fashioned-4474.jpg",
-//  "Description": "An Old Fashioned is a classic sweet whiskey cocktail",
- //   "List of ingredients": "bourbon, bitters, raw sugar, cherry garnish"
-  //},`
