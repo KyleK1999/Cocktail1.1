c4cc8f2e741ef475baa5e8d4ebc8b072d3a45fa5
Kyle <kylekibbe99@gmail.com>
2023-11-15 16:59:05 -0800

    fixed file path for json

--- src/assets/js/images/index.js
+++ src/assets/js/images/index.js
@@ -1,59 +1,42 @@
-window.addEventListener("DOMContentLoaded", getAndRenderRandomDrink);
-
-// URL definitions
-const url = "https://www.thecocktaildb.com/api/json/v1/1/";
-const randomDrinkURL = "random.php";
-const alcoholicFilterURL = "filter.php?a=Alcoholic";
-const nonAlcoholicFilterURL = "filter.php?a=Non_Alcoholic";
-
-// Fetch functions
-function fetchDrink(drinkURL) {
-    return fetch(drinkURL)
-      .then(response => response.json())
-      .then(data => data.drinks[0]);
-}
-
-function getRandomDrink(){
-  return fetchDrink(url + randomDrinkURL);
-}
-
-// Initial fetch and log
-fetchDrink(url + randomDrinkURL).then(drinksArray => {
-    console.log(drinksArray);
+window.addEventListener("DOMContentLoaded", () => {
+    renderRandomDrink();
 });
 
-// DOM selectors
-const fetchButton = document.querySelector("button");
-const hoverButton = document.querySelector("button");
-const drinkImage = document.getElementById("DrinkImage");
-const drinkDescription = document.getElementById("drinkDescription");
-const drinkInstructions = document.getElementById("strInstructions");
+// Fetch drinks data from local JSON file
+function fetchDrinks() {
+    return fetch("../../db.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => data.drinks)
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
 
 // Event listeners
-hoverButton.addEventListener("mouseover", () => {
-    hoverButton.style.color = "red";
-});
-
-hoverButton.addEventListener("mouseout", () => {
-    hoverButton.style.color = "white";
-});
-
-fetchButton.addEventListener("click", () => {
-    getRandomDrink().then(drinkObj => {
-      renderRandomDrink(drinkObj);
-    });
-});
+const fetchButton = document.querySelector("button");
+fetchButton.addEventListener("click", renderRandomDrink);
 
 // Render functions
-function renderRandomDrink(drinkObj) {
-  console.log("Rendered Drink:", drinkObj);
-  drinkImage.src = drinkObj.strDrinkThumb;
-  drinkDescription.textContent = drinkObj.strDrink;
-  drinkInstructions.textContent = drinkObj.strInstructions;
+function renderRandomDrink() {
+    fetchDrinks().then(drinksArray => {
+        if (!drinksArray) return;
+        const drink = getRandomDrink(drinksArray);
+        updateDrinkDetails(drink);
+    });
 }
 
-function getAndRenderRandomDrink() {
-  getRandomDrink().then(drinkObj => {
-    renderRandomDrink(drinkObj);
-  });
+function getRandomDrink(drinksArray) {
+    const randomIndex = Math.floor(Math.random() * drinksArray.length);
+    return drinksArray[randomIndex];
+}
+
+function updateDrinkDetails(drink) {
+    document.getElementById("DrinkImage").src = drink["Image URL"];
+    document.getElementById("drinkDescription").textContent = drink["Description"];
+    document.getElementById("strInstructions").textContent = "Ingredients: " + drink["List of ingredients"];
 }
